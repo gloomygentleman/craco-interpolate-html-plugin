151585bb617f9b3c18e59f483b603aa582b90f01
WonbaeLee <gloomygentleman@gmail.com>
2019-05-17 18:12:09 +0900

    fix: using craco plugin

--- index.js
+++ index.js
@@ -3,8 +3,27 @@
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = params => config => {
-  config.plugins.push(new InterpolateHtmlPlugin(htmlWebpackPlugin, params));
+module.exports = {
+  overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env } }) => {
+    if (
+      pluginOptions &&
+      typeof pluginOptions === 'object' &&
+      pluginOptions !== null &&
+      Object.prototype.toString(pluginOptions) === '[object Object]'
+    ) {
+      if (pluginOptions.hasOwnProperty('dev') && env === 'development') {
+        webpackConfig.plugins.push(new InterpolateHtmlPlugin(htmlWebpackPlugin, pluginOptions.dev));
+      } else if (pluginOptions.hasOwnProperty('prod') && env === 'production') {
+        webpackConfig.plugins.push(
+          new InterpolateHtmlPlugin(htmlWebpackPlugin, pluginOptions.prod)
+        );
+      } else {
+        webpackConfig.plugins.push(new InterpolateHtmlPlugin(htmlWebpackPlugin, pluginOptions));
+      }
+    } else {
+      throw new Error('The craco-interpolate-html plugin is available only for object options');
+    }
 
-  return config;
+    return webpackConfig;
+  },
 };
