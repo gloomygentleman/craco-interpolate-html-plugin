f90b700809414df587f07acd34e831e407745e0f
WonbaeLee <gloomygentleman@gmail.com>
2019-05-17 18:12:16 +0900

    add: prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// .prettierrc
+// configurations : https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
