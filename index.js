0de6e89e7d6ae98e970c8c993667fb95900b0853
WonbaeLee <gloomygentleman@gmail.com>
2019-09-20 16:32:35 +0900

    fix: Modify
    HtmlWebpackPlugin of Interpolate to refer to the same object as
    existing InterpolateHtmlPlugin

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env } }) => {
-    if (
-      pluginOptions &&
-      Object.prototype.toString(pluginOptions) === '[object Object]'
-    ) {
-      webpackConfig.plugins.push(new InterpolateHtmlPlugin(htmlWebpackPlugin, pluginOptions));
+    if (pluginOptions && Object.prototype.toString(pluginOptions) === '[object Object]') {
+      const originInterpolatePlugin = webpackConfig.plugins.find(
+        d => d.replacements && d.replacements.NODE_ENV
+      );
+
+      if (!originInterpolatePlugin) {
+        console.warn('Could not find react-dev-utils/InterpolateHtmlPlugin.');
+      }
+
+      webpackConfig.plugins.push(
+        new InterpolateHtmlPlugin(originInterpolatePlugin.htmlWebpackPlugin || require('html-webpack-plugin')
+        , pluginOptions)
+      );
     } else {
       throw new Error('The craco-interpolate-html plugin is available only for object options');
     }
